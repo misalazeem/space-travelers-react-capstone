6b773b309c9bb9bf76f93f4a9c0e6ec73117e230
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-06-19 18:04:39 +0500

    fetching missions using api

--- src/components/missions.jsx
+++ src/components/missions.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchMissions } from '../redux/missions/missionSlice';
 
 function Missions() {
+  // const missionList = useSelector((state) => state.missions.Missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
   return (
     <>
       <h2>Missions</h2>
