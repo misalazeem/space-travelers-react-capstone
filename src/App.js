ced86b50e10f71e2f0427c0bf26fd9ab6b15a28a
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-06-19 15:10:29 +0500

    added redux for missions, nav edited to reflect active nav link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import NavBar from './components/NavBar';
 import Rockets from './components/rockets';
 import Missions from './components/missions';
@@ -7,14 +9,16 @@ import Profile from './components/profile';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Routes>
-        <Route path="/rockets" element={<Rockets />} />
-        <Route path="/missions" element={<Missions />} />
-        <Route path="/my-profile" element={<Profile />} />
-      </Routes>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/rockets" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/my-profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </Provider>
   );
 }
 
