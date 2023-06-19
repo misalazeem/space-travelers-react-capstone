45025f1b13c51c688d3b937493a86192fef0f340
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-06-19 21:04:11 +0100

    Add rocket file

--- src/components/rockets.jsx
+++ src/components/rockets.jsx
@@ -1,17 +1,28 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchRockets } from '../redux/rockets/rocketsSlice';
+import '../styles/rocket.css';
+import Rocket from './rocket';
 
 function Rockets() {
+  const rocketList = useSelector((state) => state.rockets.Rockets);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchRockets());
   }, [dispatch]);
   return (
-    <>
-      <h2>Rockets</h2>
-    </>
+    <div className="rocket-container">
+      {rocketList.map((rocket) => (
+        <Rocket
+          key={rocket.id}
+          id={rocket.id}
+          name={rocket.rocket_name}
+          description={rocket.description}
+          image={rocket.image}
+        />
+      ))}
+    </div>
   );
 }
 
